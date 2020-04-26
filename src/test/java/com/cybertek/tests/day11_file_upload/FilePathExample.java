e89581c672df34afd7b861740d2731ea8645b4b7
github@cybertekschool.com <github@cybertekschool.com>
2020-04-26 12:01:55 -0400

    day11

---
+++ src/test/java/com/cybertek/tests/day11_file_upload/FilePathExample.java
@@ -0,0 +1,20 @@
+package com.cybertek.tests.day11_file_upload;
+
+import org.testng.annotations.Test;
+
+public class FilePathExample {
+
+    @Test
+    public void test(){
+        //  /Users/jamaldemir/Desktop/EU2TestNGProject/src/test/resources/testfile.txt
+
+        //getting project location/path dynamicly
+        String projectPath = System.getProperty("user.dir");
+        String relativePath = "src/test/resources/testfile.txt";
+        String filePath = projectPath+"/"+relativePath;
+        System.out.println("filePath = " + filePath);
+
+
+    }
+
+}
---
+++ src/test/java/com/cybertek/tests/day11_file_upload/FileUploadTest.java
@@ -0,0 +1,80 @@
+package com.cybertek.tests.day11_file_upload;
+
+import com.cybertek.utilities.WebDriverFactory;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class FileUploadTest {
+
+    WebDriver driver;
+
+    @BeforeMethod
+    public void setUpMethod(){
+        driver = WebDriverFactory.getDriver("chrome");
+
+    }
+
+    @AfterMethod
+    public void afterMethod() throws InterruptedException {
+        Thread.sleep(2000);
+        driver.quit();
+    }
+
+    /**
+     *  go to website http://practice.cybertekschool.com/upload
+     *  upload file.txt
+     *  click upload
+     *  verify file name is displayed in the next page
+     */
+    @Test
+    public void test1(){
+        driver.get("http://practice.cybertekschool.com/upload");
+        //locating choosefile button
+        WebElement chooseFile = driver.findElement(By.name("file"));
+        //sending file
+        chooseFile.sendKeys("/Users/jamaldemir/Desktop/file.txt");
+        //clicking upload button
+        driver.findElement(By.id("file-submit")).click();
+
+        //getting name of the file
+        String actualFilename = driver.findElement(By.id("uploaded-files")).getText();
+        //verify file name is file.txt
+        Assert.assertEquals(actualFilename,"file.txt");
+
+
+
+    }
+
+    @Test
+    public void test2(){
+        driver.get("http://practice.cybertekschool.com/upload");
+        //locating choosefile button
+        WebElement chooseFile = driver.findElement(By.name("file"));
+        //sending file
+        String projectPath = System.getProperty("user.dir");
+        String relativePath = "src/test/resources/testfile.txt";
+        String filePath = projectPath+"/"+relativePath;
+        chooseFile.sendKeys(filePath);
+        //clicking upload button
+        driver.findElement(By.id("file-submit")).click();
+
+        //getting name of the file
+        String actualFilename = driver.findElement(By.id("uploaded-files")).getText();
+        //verify file name is file.txt
+        Assert.assertEquals(actualFilename,"file.txt");
+
+
+
+    }
+
+
+
+
+
+
+}
