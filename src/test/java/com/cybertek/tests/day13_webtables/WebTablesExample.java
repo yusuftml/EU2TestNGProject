017ea3a98f03945ecaf42ed50fdfeb11c1639600
github@cybertekschool.com <github@cybertekschool.com>
2020-05-10 12:59:05 -0400

    day14

--- src/test/java/com/cybertek/tests/day13_webtables/WebTablesExample.java
+++ src/test/java/com/cybertek/tests/day13_webtables/WebTablesExample.java
@@ -136,6 +136,18 @@ public class WebTablesExample {
 
     }
 
+    @Test
+    public void getCellInRelationToanotherCellInSameRow(){
+
+        String firstName = "John";
+        String xpath = "//table[@id='table1']//td[.='"+firstName+"']/../td[3]";
+        WebElement email = driver.findElement(By.xpath(xpath));
+        System.out.println(email.getText());
+
+    }
+
+
+
     private int getNumberOfColumns() {
         List<WebElement> headers = driver.findElements(By.xpath("//table[@id='table1']//th"));
         return headers.size();
---
+++ src/test/java/com/cybertek/tests/day14_properties_driver_tests/SingletonTest.java
@@ -0,0 +1,18 @@
+package com.cybertek.tests.day14_properties_driver_tests;
+
+import org.testng.annotations.Test;
+
+public class SingletonTest {
+
+    @Test
+    public void test(){
+        String s1 = Singleton.getInstance();
+        //100 lines of code
+        String s2 = Singleton.getInstance();
+
+        System.out.println("s1 = " + s1);
+        System.out.println("s2 = " + s2);
+
+
+    }
+}
